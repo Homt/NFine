f8348cd12ce40adefe62e3471fa93804503775e6
carryDU <dudan_3643@outlook.com>
2017-07-24 21:11:32 +0800

    新增

---
+++ NFine.API/Controllers/TaskController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using NFine.Application;
+using NFine.Application.MessageManage;
+using NFine.Application.SystemManage;
+using NFine.Application.TaskManage;
+using NFine.API.Models;
+using NFine.Code;
+using NFine.Domain._03_Entity.CustomerManage;
+
+namespace NFine.API.Controllers
+{
+    public class TaskController : ApiController
+    {
+        /// <summary>
+        /// 任务列表
+        /// </summary>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageSize">数量</param>
+        /// <param name="bank">银行</param>
+        /// <param name="schedule">进度</param>
+        /// <param name="keyword">关键字</param>
+        /// <param name="startdt">开始时间</param>
+        /// <param name="enddt">结束时间</param>
+        /// <param name="userid">用户id</param>
+        /// <returns></returns>
+        public ApiResult<dynamic> PostTaskList(int? pageIndex, int? pageSize, string bank, string schedule, string keyword, string startdt, string enddt, string userid)
+        {
+            ApiResult<dynamic> api = new ApiResult<dynamic>();
+            try
+            {
+                int pageindex = pageIndex ?? 0;// Common.GetInt("pageIndex", 0);// jsondata.pageIndex ?? 0;
+                int pagesize = pageSize ?? 0;// Common.GetInt("pageSize", 0);// jsondata.pageSize ?? 0;
+                TaskApp taskApp = new TaskApp();
+                Pagination pagination = new Pagination();
+                pagination.page = pageindex;
+                pagination.rows = pagesize;
+                pagination.sidx = "F_CreatorTime";
+                pagination.sord = "desc";
+                var result = taskApp.GetList(pagination, keyword, schedule, startdt, enddt, userid, bank);
+                CustomerApp customerApp = new CustomerApp();
+                List<dynamic> list = new List<dynamic>();
+                foreach (var item in result)
+                {
+                    var entity = customerApp.GetForm(item.F_CustomerId);
+                    list.Add(entity);
+                }
+                api.Message = "获取成功";
+                api.Status = true;
+                api.Result = list;
+                return api;
+            }
+            catch (Exception e)
+            {
+                api.Message = e.Message;
+                return api;
+            }
+        }
+       /// <summary>
+       /// 任务详情
+       /// </summary>
+       /// <param name="id">任务id</param>
+       /// <returns></returns>
+        public ApiResult<dynamic> Details(string id)
+        {
+            ApiResult<dynamic> api = new ApiResult<dynamic>();
+            try
+            {
+                TaskApp taskApp = new TaskApp();
+                var taskData = taskApp.GetForm(id);
+                CustomerApp customerApp = new CustomerApp();
+                var customerData = customerApp.GetForm(taskData.F_CustomerId);
+                UserApp userApp = new UserApp();
+                var userData = userApp.GetForm(taskData.F_UserId);
+                var data = new
+                {
+                    F_Shop_Name = customerData.F_Shop_Name,
+                    F_Shop_Address = customerData.F_Shop_Address,
+                    F_Name = customerData.F_Name,
+                    F_Tel = customerData.F_Tel,
+                    F_User_Name = userData.F_RealName,
+                    F_Task_Time = taskData.F_CreatorTime,
+                    F_User_Tel = userData.F_MobilePhone,
+                    F_Status = GetName(taskData.F_Status)
+                };
+                api.Message = "获取成功";
+                api.Status = true;
+                api.Result = data;
+                return api;
+            }
+            catch (Exception e)
+            {
+                api.Message = e.Message;
+                return api;
+            }
+        }
+        /// <summary>
+        /// 获取数据字典名称
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private string GetName(string id)
+        {
+            ItemsDetailApp itemsDetailApp = new ItemsDetailApp();
+            var data = itemsDetailApp.GetForm(id);
+            if (data != null)
+                return data.F_ItemName;
+            return string.Empty;
+        }
+
+       /// <summary>
+       /// 提交任务
+       /// </summary>
+       /// <param name="customerId">客户id</param>
+       /// <param name="userId">用户id</param>
+       /// <param name="lat">纬度</param>
+       /// <param name="lng">经度</param>
+       /// <returns></returns>
+        public ApiResult<dynamic> PostCustomerData(string customerId,string userId,float lat,float lng)
+        {
+            ApiResult<dynamic> api = new ApiResult<dynamic>();
+            try
+            {
+                CustomerApp customerApp = new CustomerApp();
+                var customerModel = customerApp.GetForm(customerId);
+                customerModel.F_LastModifyTime = DateTime.Now;
+                customerModel.F_LastModifyUserId = userId;
+                customerModel.F_Lat = lat;
+                customerModel.F_Lng = lng;
+                customerApp.SubmitFormAPI(customerModel, customerModel.F_Id);
+                api.Message = "提交成功";
+                api.Status = true;
+                return api;
+            }
+            catch (Exception e)
+            {
+                api.Message = e.Message;
+                return api;
+            }
+        }
+    }
+}
---
+++ NFine.API/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using NFine.Application;
+using NFine.Application.SystemManage;
+using NFine.Application.SystemSecurity;
+using NFine.API.Attribute;
+using NFine.API.Models;
+using NFine.Code;
+using NFine.Domain.Entity.SystemManage;
+using NFine.Domain.Entity.SystemSecurity;
+
+namespace NFine.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        /// <summary>
+        /// 用户登录
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <param name="userPwd">密码</param>
+        /// <returns></returns>
+        public ApiResult<dynamic> Login(string userName, string userPwd)
+        {
+            ApiResult<dynamic> api = new ApiResult<dynamic>();
+            try
+            {
+                userPwd = Md5.md5(userPwd, 32).ToLower();
+                UserEntity userEntity = new UserApp().CheckLogin(userName, userPwd);
+                if (userEntity != null)
+                {
+                    OperatorModel operatorModel = new OperatorModel();
+                    operatorModel.UserId = userEntity.F_Id;
+                    operatorModel.UserCode = userEntity.F_Account;
+                    operatorModel.UserName = userEntity.F_RealName;
+                    operatorModel.CompanyId = userEntity.F_OrganizeId;
+                    operatorModel.DepartmentId = userEntity.F_DepartmentId;
+                    operatorModel.RoleId = userEntity.F_RoleId;
+                    operatorModel.LoginIPAddress = Net.Ip;
+                    operatorModel.LoginIPAddressName = Net.GetLocation(operatorModel.LoginIPAddress);
+                    operatorModel.LoginTime = DateTime.Now;
+                    operatorModel.LoginToken = DESEncrypt.Encrypt(Guid.NewGuid().ToString());
+                    if (userEntity.F_Account == "admin")
+                    {
+                        operatorModel.IsSystem = true;
+                    }
+                    else
+                    {
+                        operatorModel.IsSystem = false;
+                    }
+                    api.Message = "登录成功";
+                    api.Status = true;
+                    api.Result = operatorModel;
+                    return api;
+                }
+                else
+                {
+                    api.Message = "用户名或密码有误";
+                    return api;
+                }
+            }
+            catch (Exception e)
+            {
+                api.Message = e.Message;
+                return api;
+            }
+        }
+       /// <summary>
+       /// 修改密码
+       /// </summary>
+       /// <param name="userId">用户id</param>
+       /// <param name="password">原密码</param>
+       /// <param name="newpassword">新密码</param>
+       /// <param name="repassword">确认新密码</param>
+       /// <returns></returns>
+        public ApiResult<dynamic> RevisePassword(string userId, string password, string newpassword, string repassword)
+        {
+            ApiResult<dynamic> api = new ApiResult<dynamic>();
+            try
+            {
+                string userid = userId ?? string.Empty;// Common.GetString("userId");
+                string pwd = password ?? string.Empty;// Common.GetString("password");
+                string newpwd = newpassword ?? string.Empty;// Common.GetString("newpassword");
+                string repwd = repassword ?? string.Empty;// Common.GetString("repassword");
+                UserLogOnApp userLogOnApp = new UserLogOnApp();
+                var userLogOn = userLogOnApp.GetForm(userid);
+                var oldpswd = Md5.md5(DESEncrypt.Encrypt(Md5.md5(pwd, 32).ToLower(), userLogOn.F_UserSecretkey).ToLower(), 32).ToLower();
+                if (userLogOn.F_UserPassword != oldpswd)
+                {
+                    api.Message = "原密码有误";
+                    return api;
+                }
+                if (newpwd != repwd)
+                {
+                    api.Message = "确认密码和新密码不一致";
+                    return api;
+                }
+                userLogOnApp.ModifyPassword(newpwd, userLogOn.F_Id);
+                api.Message = "修改成功";
+                api.Status = true;
+                return api;
+            }
+            catch (Exception e)
+            {
+                api.Message = e.Message;
+                return api;
+            }
+        }
+
+    }
+}
